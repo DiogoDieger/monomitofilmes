a2e7e6a673e488a08cc4bc6f34e1f41c61acc067
Caio Timoteo <caiocesar.wk@gmail.com>
2025-02-17 19:54:59 -0300

    Update index.js

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -14,7 +14,7 @@ export const Header = () => {
         <div>
           <Link href="/">
             <h1>
-              Monomito Filmes
+              Monomito Filmes ssss
             </h1>
           </Link>
         </div>
